13556d664e88d13c1da778f34f376dc2c56a02cd
Yuvraj Singh <101858632+05-yuvraj-singh@users.noreply.github.com>
2023-09-16 11:45:25 +0530

    Update NewUser.jsx

--- client/src/NewUser.jsx
+++ client/src/NewUser.jsx
@@ -9,7 +9,7 @@ const NewUser = () => {
       setSignInSucessful(false)}, 3000);
   }
   function putData() {
-    fetch("https://deploying-test-api.vercel.app/signup", {
+    fetch("https://api-deployed.onrender.com/signup", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
