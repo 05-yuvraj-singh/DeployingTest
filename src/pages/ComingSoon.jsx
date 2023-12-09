91be74efafcb6237badcff9f47a36dac3f2c9503
05-yuvraj-singh <yuvrajsingh532004@gmail.com>
2023-12-09 22:54:17 +0530

    hextrend project initated

---
+++ src/pages/ComingSoon.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Stack, Heading, Text } from '@chakra-ui/react';
+
+const ComingSoon = () => {
+  return (
+    <Stack
+      spacing={8}
+      p={{ base: 4, md: 8 }}
+      borderWidth="1px"
+      borderRadius="md"
+      maxW="100%" // Set max width to 100% for full-width on all screen sizes
+      mx="auto"
+      textAlign="center"
+      justifyContent="center"
+      align="center"
+      h="100vh"
+      bgColor="#F5E1FF"
+    >
+      <Heading
+        as="h1"
+        size="2xl"
+        mt={{ base: 8, md: 0 }}
+        color="#2980b9" 
+        letterSpacing="0.1em"
+      >
+        COMING SOON
+      </Heading>
+      <Text fontSize="lg" color="#333333">
+        Welcome to Hextrend! The website is under construction and will be
+        live soon. Get ready for an exciting experience!
+      </Text>
+    </Stack>
+  );
+};
+
+export default ComingSoon;
